a0018eee1307b814aa443c5c515acd3ab979c955
Nick Whelan <nickwhelan5@gmail.com>
2017-05-06 15:27:03 -0400

    ships and thrusters

--- Venture/Assets/AI/Ship/Ship.cs
+++ Venture/Assets/AI/Ship/Ship.cs
@@ -6,36 +6,95 @@ public class Ship : MonoBehaviour {
     public Blackboard general = new Blackboard();
     public Inventory inventory = new Inventory();
 
-    public Thruster[] backThrusters, frontThrusters, upThrusters, downThrusters;
+    public Thruster[] thrusters;
+    public List<int>
+        backThrusters, frontThrusters, upThrusters, downThrusters, leftThrusters, rightThrusters,
+        turnLeftThrusters, turnRightThrusters, turnUpThrusters, turnDownThrusters;
 
 
     public Rigidbody rigidbody;
 
-    public Vector3 goal, target;
+    Vector3 start;
+    public Transform goal, target;
     public float fuel, currentVel , desiredVel;
 
-    void start() {
-        general.Add(, new BlackboardValue() { Name = "ParentAgent", Value = _ParentAgent });
+    void Start() {
+        start = transform.position;
+        target = goal;
+        target.position = new Vector3(target.position.x, target.position.y, target.position.z - (target.localScale.z*2));
+
+        for (int i = 0; i < thrusters.Length; i++) {
+            if (thrusters[i].isABackThruster) {
+                backThrusters.Add(i);
+            }
+            else if (thrusters[i].isAFrontThruster) {
+                frontThrusters.Add(i);
+            }
+            else if (thrusters[i].isAUpThruster) {
+                upThrusters.Add(i);
+            }
+            else if (thrusters[i].isADownThruster) {
+                downThrusters.Add(i);
+            }
+            else if (thrusters[i].isALeftThruster) {
+                leftThrusters.Add(i);
+            }
+            else if (thrusters[i].isARightThruster) {
+                rightThrusters.Add(i);
+            }
+
+            if (thrusters[i].isATurnLeftThruster) {
+                turnLeftThrusters.Add(i);
+            }
+            else if (thrusters[i].isATurnRightThruster) {
+                turnRightThrusters.Add(i);
+            }
+            else if (thrusters[i].isATurnUpThruster) {
+                turnUpThrusters.Add(i);
+            }
+            else if (thrusters[i].isATurnDownThruster) {
+                turnDownThrusters.Add(i);
+            }
+        }
     }
     
     // Use this for initialization
     public void Launch(Station station, Specialisations specialisation, Inventory inventory) {
-
-
     }
-
-
-    
-
 	// Update is called once per frame
 	void FixedUpdate () {
-        currentVel = rigidbody.velocity.z;
-        if (currentVel < desiredVel)
+        transform.LookAt(target);
+        if (Vector3.Distance(transform.position, target.position) > 0.5)
         {
-            foreach (Thruster thruster in downThrusters)
+            if (Vector3.Distance(target.position,transform.position) > Vector3.Distance((target.position / 2),target.position))
             {
-                thruster.Thrust();
+                Forward();
+            }
+            else if (Vector3.Distance(transform.position, target.position) < (Vector3.Distance(start, target.position) / 2))
+            {
+                print("Back");
+                Back();
             }
         }
 	}
+
+    void Forward() {
+        for (int i = 0; i < backThrusters.Count; i++)
+        {
+            thrusters[backThrusters[i]].Thrust();
+        }
+    }
+    void Back() {
+        for (int i = 0; i < frontThrusters.Count; i++)
+        {
+            thrusters[frontThrusters[i]].Thrust();
+        }
+    }
+    void turnRight() { }
+    void turnLeft() {
+        for (int i = 0; i < turnLeftThrusters.Count; i++) {
+            thrusters[turnLeftThrusters[i]].Thrust();
+        }
+    }
+
 }
--- Venture/Assets/AI/Ship/Thruster.cs
+++ Venture/Assets/AI/Ship/Thruster.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Thruster : MonoBehaviour {
     public float thrust;
     public Ship ship;
+    public bool
+        isABackThruster, isAFrontThruster, isAUpThruster, isADownThruster, isALeftThruster, isARightThruster,
+        isATurnLeftThruster, isATurnRightThruster, isATurnUpThruster, isATurnDownThruster;
     public void Thrust()
     {
            ship.rigidbody.AddForceAtPosition(transform.forward * thrust, transform.position);
