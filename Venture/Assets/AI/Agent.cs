590897c5f106f9bad86fb1a3670054430f634b7b
Nick Whelan <nickwhelan5@gmail.com>
2017-05-06 03:52:38 -0400

    More work
    -added ships
    -worked on agents
    -worked employee

--- Venture/Assets/AI/Agent.cs
+++ Venture/Assets/AI/Agent.cs
@@ -13,37 +13,57 @@ public enum Specialisations {
 }
 
 public class Agent : MonoBehaviour {
-    const string  Inventory = "inventory", Employees = "Employees", CostPerMonth = "CostPerMonth";
-    public Blackboard AgentBlackboard;
-    public Inventory inventory;
+    public static class Literals{
+        const string Employees = "Employees", 
+                    CostPerMonth = "CostPerMonth";
 
-    // Use this for initialization
-    public void Launch() {
-        // ask station what resource. 
     }
-	
+
+
+    public Blackboard general = new Blackboard();
+    
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
+    
     public static Agent Factory() {
         Specialisations Specialisation = new Specialisations();
         Specialisation = (Specialisations) UnityEngine.Random.Range(0, Enum.GetNames(typeof(Specialisations)).Length);
 
         Agent agent = new Agent();
-        agent.AgentBlackboard.Add("Name", new BlackboardValue() { Name = "Name", Value = "Agent Name" });
-        agent.AgentBlackboard.Add("Inventory", new BlackboardValue() { Name = "Inventory", Value = agent.inventory });
-        agent.inventory.Start();
+        agent.general.Add("Name", new BlackboardValue() { Name = "Name", Value = "Agent Name" });
         List<Employee> Employees = new List<Employee>();
         int randomNumEmployees = UnityEngine.Random.Range(1, 100);
+
+        //setting valuse for ui
+        float EmployeesEfficiencyMax=0, EmployeesEfficiencyMin=3, EmployeesEfficiencyMean=0, EmployeesEfficiencyMedian=0, EmployeesEfficiencyMode=0;
+        Dictionary<string, float> ModeNumbers = new Dictionary<string, float>();
         for (int i = 0; i < randomNumEmployees; i++) {
             Employees.Add(new Employee());
-            Employees[i].Birth(Specialisation,);
-        }
-        agent.AgentBlackboard.Add("Employees", new BlackboardValue() { Name = "Employees", Value = Employees });
-        agent.AgentBlackboard.Add("CostPerMonth", new BlackboardValue() { Name = "CostPerMonth", Value = Employees.Count * UnityEngine.Random.Range(4000,6000)});
+            Employees[i].Birth(Specialisation,agent);
+            EmployeesEfficiencyMean += (float) Employees[i].EmployeeInfo["Efficiency"].Value;
+            EmployeesEfficiencyMax = ((float)Employees[i].EmployeeInfo["Efficiency"].Value > EmployeesEfficiencyMax) ? (float)Employees[i].EmployeeInfo["Efficiency"].Value : EmployeesEfficiencyMax;
+            EmployeesEfficiencyMin = ((float)Employees[i].EmployeeInfo["Efficiency"].Value < EmployeesEfficiencyMin) ? (float)Employees[i].EmployeeInfo["Efficiency"].Value : EmployeesEfficiencyMin;
 
+            if (ModeNumbers.ContainsKey((string)Employees[i].EmployeeInfo["Efficiency"].Value)){
+                ModeNumbers[(string)Employees[i].EmployeeInfo["Efficiency"].Value]++;
+            }
+            else{
+                ModeNumbers.Add((string)Employees[i].EmployeeInfo["Efficiency"].Value, 1);
+            }
+        }
+        EmployeesEfficiencyMean /= Employees.Count;
+        EmployeesEfficiencyMedian = (float)Employees[Employees.Count/2].EmployeeInfo["Efficiency"].Value;
+        float ModeNumbersTemp = 0;
+        foreach (KeyValuePair<string, float> pair in ModeNumbers) {
+            if (ModeNumbersTemp > pair.Value) {
+                EmployeesEfficiencyMode = pair.Value;
+            }
+        }
+
+        agent.general.Add("Employees", new BlackboardValue() { Name = "Employees", Value = Employees });
+        agent.general.Add("CostPerMonth", new BlackboardValue() { Name = "CostPerMonth", Value = Employees.Count * UnityEngine.Random.Range(4000,6000)});
         return agent;
     }
 
