34da02068d45a48b46d242497a0f30ec72630ff7
aidandearing <aidan.dearing@live.com>
2017-05-06 13:07:57 -0400

    Station
    More station stuff.

---
+++ Venture/Assets/UI/Lib/UIInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseClick
+{
+    public float startTime = 0;
+    public Vector3 startPosition = Vector3.zero;
+
+}
+
+public class UIInterface : MonoBehaviour
+{
+    void Start()
+    {
+        Blackboard.Global.Add(Consts.Globals.UIInterface, new BlackboardValue() { Name = Consts.Globals.UIInterface, Value = this });
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
