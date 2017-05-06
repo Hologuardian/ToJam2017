e05a37b4815be0b27ad2397e92e8717e236b9d09
Hologuardian <namaeskram@gmail.com>
2017-05-06 12:02:35 -0400

    Asteroids

---
+++ Venture/Assets/Asteroids/Asteroid.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    Color c = Random.ColorHSV();
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = c;
+        Gizmos.DrawSphere(transform.position, 1.0f);
+    }
+}
