5d750ec37723f825cfa5218278414cd35d4dab31
Nick Whelan <nickwhelan5@gmail.com>
2017-05-06 22:09:10 -0400

    Broke Everything and tanked it

---
+++ Venture/Assets/AI/Ship/Nav.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Nav : MonoBehaviour {
+    float waitTime = 0.05f;
+    float currentTime;
+    RaycastHit rayHit;
+    // Use this for initialization
+    void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit rayHit = new RaycastHit();
+        if (Physics.Raycast(new Vector3(transform.position.x + (transform.localScale.x/2), transform.position.y, transform.position.z), transform.forward, out rayHit, 100))
+        {
+            FindEdge();
+        }
+        else if (Physics.Raycast(transform.position, transform.forward, out rayHit, 100))
+        {
+            FindEdge();
+        }
+        else if (Physics.Raycast(new Vector3(transform.position.x - (transform.localScale.x / 2), transform.position.y, transform.position.z), transform.forward, out rayHit, 100))
+        {
+            FindEdge();
+        }
+
+    }
+    void FindEdge() {
+        transform.Rotate(new Vector3(0, 1, 0), 45 * Mathf.Deg2Rad);
+    }
+}
--- Venture/Assets/AI/Ship/Ship.cs
+++ Venture/Assets/AI/Ship/Ship.cs
@@ -1,117 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Ship : MonoBehaviour {
+public class Ship : MonoBehaviour
+{
     public Blackboard general = new Blackboard();
     public Inventory inventory = new Inventory();
-    Specialisations shipsSpecialisation = new Specialisations();
-    public bool arrived= false;
-    public Thruster[] thrusters;
-    public List<int>
-        backThrusters, frontThrusters, upThrusters, downThrusters, leftThrusters, rightThrusters,
-        turnLeftThrusters, turnRightThrusters, turnUpThrusters, turnDownThrusters;
+    public AsteroidBelt asteroidBelt;
+    bool isInitialize = false;
 
+    public NavMeshAgent navMeshAgent;
 
-    public Rigidbody rigidbody;
+    void Start()
+    {
 
-    Vector3 start;
-    public Transform goal, target;
-    public float fuel, currentVel, desiredVel, stoppingDistance, dist; 
-
-    void Start() {
-        start = transform.position;
-        target = goal;
-        target.position = new Vector3(target.position.x, target.position.y, target.position.z - (target.localScale.z*2));
-
-
-        for (int i = 0; i < thrusters.Length; i++) {
-            if (thrusters[i].isABackThruster) {
-                backThrusters.Add(i);
-            }
-            else if (thrusters[i].isAFrontThruster) {
-                frontThrusters.Add(i);
-            }
-            else if (thrusters[i].isAUpThruster) {
-                upThrusters.Add(i);
-            }
-            else if (thrusters[i].isADownThruster) {
-                downThrusters.Add(i);
-            }
-            else if (thrusters[i].isALeftThruster) {
-                leftThrusters.Add(i);
-            }
-            else if (thrusters[i].isARightThruster) {
-                rightThrusters.Add(i);
-            }
-
-            if (thrusters[i].isATurnLeftThruster) {
-                turnLeftThrusters.Add(i);
-            }
-            else if (thrusters[i].isATurnRightThruster) {
-                turnRightThrusters.Add(i);
-            }
-            else if (thrusters[i].isATurnUpThruster) {
-                turnUpThrusters.Add(i);
-            }
-            else if (thrusters[i].isATurnDownThruster) {
-                turnDownThrusters.Add(i);
-            }
-        }
     }
-    
+
+    void Initialize()
+    {
+        print("running");
+        isInitialize = true;
+        navMeshAgent.destination = asteroidBelt.roids[Random.Range(0, asteroidBelt.roids.Count - 1)].transform.position;
+    }
+
     // Use this for initialization
-    public void Launch(Specialisations specialisation) {
-        
+    public void Launch(Specialisations specialisation)
+    {
+
     }
 
-// Update is called once per frame
-void FixedUpdate () {
-        transform.LookAt(target);
-        dist = Vector3.Distance(transform.position, target.position);
-        currentVel = rigidbody.velocity.z;
-        if (dist > 3 && !arrived)
-        {
-            if (dist > Vector3.Distance((target.position / 2), target.position))
-            {
-                Forward();
-            }
-            else if (dist < (Vector3.Distance(start, target.position) / 2))
-            {
-                if (rigidbody.velocity.z < 0.5) {
-                    Forward();
-                }
-                else
-                {
-                    Back();
-                }
-            }
-            if (dist < 4) {
-                rigidbody.velocity = Vector3.zero;
-                arrived = true;
-            }
-        }
-        if (arrived) {
-            rigidbody.velocity = Vector3.zero;
-        }
-	}
-    void Forward() {
-        for (int i = 0; i < backThrusters.Count; i++)
-        {
-            thrusters[backThrusters[i]].Thrust();
-        }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
     }
-    void Back() {
-        
-        for (int i = 0; i < frontThrusters.Count; i++)
+    void Update()
+    {
+        navMeshAgent.destination = asteroidBelt.roids[Random.Range(0, asteroidBelt.roids.Count - 1)].transform.position;
+        if (!isInitialize)
         {
-            thrusters[frontThrusters[i]].Thrust();
-        }
-    }
-    void turnRight() { }
-    void turnLeft() {
-        for (int i = 0; i < turnLeftThrusters.Count; i++) {
-            thrusters[turnLeftThrusters[i]].Thrust();
+            Initialize();
         }
     }
 
