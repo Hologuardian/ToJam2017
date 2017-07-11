b1ce86edf322859763d36214d311f22cf24720ae
aidandearing <aidan.dearing@live.com>
2017-07-11 14:53:31 -0400

    Attempting Update of ignore .csproj

--- Venture/Assets/Station/Src/Modules/Module.cs
+++ Venture/Assets/Station/Src/Modules/Module.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Module : MonoBehaviour {
+namespace Station
+{
+    public class Hardpoint : MonoBehaviour
+    {
+        public Module module;
+        public Hardpoint connection;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public class Submodule : MonoBehaviour
+    {
+        public Module module;
+    }
+
+    public class Module : MonoBehaviour
+    {
+        public Hardpoint[] hardpoints;
+        public Submodule[] submodules;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
 }
