806d3cacbbbe78853bc064cd15970fdb68056c94
aidandearing <aidan.dearing@live.com>
2017-07-11 19:37:03 -0400

    Module Logic Framework
    Defined the beginnings of Volatile and Unity facing Module logic, and class structuring.

--- Venture/Assets/Station/Src/Module.cs
+++ Venture/Assets/Station/Src/Module.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Engine.Src;
 
 namespace Station
 {
+    // TODO place in own Hardpoint.cs
     public class Hardpoint : MonoBehaviour
     {
         public Module module;
         public Hardpoint connection;
     }
 
+    // TODO place in own Submodule.cs
     public class Submodule : MonoBehaviour
     {
         public Module module;
+    }
+
+    public class VolatileModule : VolatileObject
+    {
+        // Properties
+        // Ensure proper care is taken to considering which must be Mementos and which can be generic
+
+        // Methods
+        /// <summary>
+        /// Update must be thread safe
+        /// </summary>
+        public void Update()
+        {
+            lock(this)
+            {
+                // TODO generic module logic
+                this.OverrideUpdate();
+            }
+        }
+
+        /// <summary>
+        /// This method is the overridable method used to give VolatileModule child classes actual logic, while gauranteeing thread safety, as Update locks this before calling OverrideUpdate
+        /// </summary>
+        public abstract void OverrideUpdate();
     }
 
     public class Module : MonoBehaviour
