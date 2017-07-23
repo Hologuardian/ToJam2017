f54aa4420871600f4dae0fd21e3ddab2a1ffa551
aidandearing <aidan.dearing@live.com>
2017-07-23 09:29:04 -0400

    VolatileModule Update
    Save the many TODOs, requiring logic not yet implemented, this Module goes about performing the basics of updating, as I know them.
    Pressurisation is not implemented
    Distribution is not implemented
    Power distribution is implemented
    Events theoretically are implemented (if they act as requests)
    Update radiating with sequencing is implemented as far as logic is concerned (small hiccup with the not actually having access to put new update tasks on the taskmanager)

--- Venture/Assets/Engine/Src/VolatileObject.cs
+++ Venture/Assets/Engine/Src/VolatileObject.cs
@@ -7,6 +7,28 @@ namespace Assets.Engine.Src
 {
     public class VolatileObject
     {
+        [Flags]
+        public enum State
+        {
+            /// <summary>
+            /// This VolatileObject is currently locked
+            /// </summary>
+            Locked = 1,
+            /// <summary>
+            /// This VolatileObject is being accessed
+            /// </summary>
+            Accessed = 2,
+            /// <summary>
+            /// This VolatileObject is currently affected by Unity
+            /// </summary>
+            Unity = 4,
+            /// <summary>
+            /// This VolatileObject is currently affected by the threaded environment
+            /// </summary>
+            Threaded = 8
+        };
+        public State state;
+
         public List<Request> requests;
 
         public void Request(Request request)
--- Venture/Assets/Station/Src/VolatileModule.cs
+++ Venture/Assets/Station/Src/VolatileModule.cs
@@ -12,6 +12,7 @@ namespace Assets.Station.Src
     {
         // Properties
         // Ensure proper care is taken to considering which must be Mementos and which can be generic
+        public int updateSequence = 0;
         /// <summary>
         /// All modules have a name, this is the name of their table in the database, and their unique name in game data
         /// </summary>
@@ -27,7 +28,7 @@ namespace Assets.Station.Src
         /// </summary>
         public Size Size { get; protected set; }
         /// <summary>
-        /// The dimensions in cubes of the size specified with the this.size property.
+        /// The dimensions in cubes of the size specified with the this.Size property.
         /// </summary>
         public Vector3 Dimensions { get; protected set; }
         /// <summary>
@@ -43,7 +44,7 @@ namespace Assets.Station.Src
         /// </summary>
         public float MaximumPressurisation { get; protected set; }
         /// <summary>
-        /// The percentage of incoming power that is lost in travel across this module.
+        /// The percentage of incoming power that is kept in travel across this module.
         /// </summary>
         public float LineLoss { get; protected set; }
 
@@ -131,34 +132,52 @@ namespace Assets.Station.Src
         {
             lock (this)
             {
+                // This VolatileObject is currently locked by the threaded environment, and its properties are being accessed
+                state = State.Locked | State.Threaded | State.Accessed;
+
                 // RESET ----------------------------------------------------------------------------------
                 // Reset all parameters that reset every update
                 EnergyProduction = 0;
 
                 // REQUESTS -------------------------------------------------------------------------------
-                // First, all update requests, as well as a variety of others come from the hardpoints, in this manner one module speaks to the others hardpoints locking them
-                // Since each talks to the others, this allows for 2 way dialogue without causing either to lock the other with direct module to module talk.
-                // To do that we need to get all the hardpoint requests from this object, and take all their requests.
+                // First, all update requests, as well as a variety of others come from the hardpoints, in this manner one module speaks to the other's hardpoints locking them
+                // Since each talks to the other's, this allows for 2 way dialogue without causing either to lock the other with direct module to module access.
+                // To do that we need to get all the hardpoint requests from this object, and ensure this module has them in its requests.
                 foreach (Hardpoint hardpoint in UnityObject.hardpoints)
                 {
                     // This prevents any race conditions
                     lock (hardpoint.threaded)
                     {
+                        hardpoint.threaded.state = State.Locked | State.Threaded | State.Accessed;
                         // Take all the requests from the hardpoint
                         foreach (Request request in hardpoint.threaded.requests)
                         {
+                            // In a non-threaded environment I would simply perform the Do on this request, rather than moving it into another list, and wasting computation time.
+                            // The problem is, if I do that, the Dos might go ahead and access and lock neighbouring hardpoints, if that occurs the entire concept of only locking hardpoints
+                            // for a defined amount of time (long enough to iterate across the list of requests and put them in a different list) becomes locking them for an undefined amount of time
+                            // while the request does whatever it does. (This could lead to a deadlock.)
                             requests.Add(request);
+
+                            // Deadlock condition avoided without Do()
+                            // Module A locks a hardpoint and begins Doing its requests
+                            // Module B locks a hardpoint and begins Doing its requests
+                            // Module A is connected to Module B
+                            // Module A does a request that wants to add a request to Module B's locked hardpoint (This locks Module B's hardpoint)
+                            // Module B does a request that wants to add a request to Module A's locked hardpoint (Since both are locked and waiting for the other, neither will unlock)
+                            // Obviously this could be avoided with a "whoever locked first bail, a missed update can just be done later"
                         }
 
                         // Clear the hardpoints requests
                         hardpoint.threaded.requests.Clear();
                     }
+                    hardpoint.threaded.state = hardpoint.threaded.state & ~State.Locked;
+                    hardpoint.threaded.state = hardpoint.threaded.state & ~State.Threaded;
+                    hardpoint.threaded.state = hardpoint.threaded.state & ~State.Accessed;
                 }
 
                 // Process all the requests
                 foreach (Request request in requests)
                 {
-                    // Requests must be fast executing pieces of logic
                     request.Do(this);
                 }
 
@@ -166,6 +185,10 @@ namespace Assets.Station.Src
                 OverridableUpdate();
 
                 // SUBMODULES -----------------------------------------------------------------------------
+                foreach(Submodule sub in UnityObject.submodules)
+                {
+                    // TODO Submodule Update
+                }
 
                 // HARDPOINTS -----------------------------------------------------------------------------
                 // First calculate all output values for each hardpoint connection
@@ -181,22 +204,30 @@ namespace Assets.Station.Src
                         request.energyIn = Mathf.Max(EnergyProduction * LineLoss, 0);
 
                     // Distribution
-
-
-                    // Events
-
+                    // TODO Inventory Distribution (Requires having an inventory)
 
                     // Queue hardpoint update, with the necessary inputs
                     hardpoint.connection.threaded.Request(request);
+
+                    // If the connected hardpoints module has a lower update sequence than this one,
+                    if (hardpoint.connection.module.threaded.updateSequence < updateSequence)
+                    {
+                        hardpoint.connection.module.threaded.updateSequence = updateSequence;
+                        // TODO queue module update (requires working taskmanager)
+                    }
                 }
 
                 // LASTPASS ------------------------------------------------------------------------------
                 // All the final updates to ensure that user data is correct
+
+                state = state & ~State.Locked;
+                state = state & ~State.Threaded;
+                state = state & ~State.Accessed;
             }
         }
 
         /// <summary>
-        /// This method is the overridable method used to give OverridableUpdate child classes actual logic, while gauranteeing thread safety, as Update locks this before calling OverridableUpdate
+        /// This method is the overridable method used to give OverridableUpdate child classes logic, while gauranteeing thread safety, as Update locks this before calling OverridableUpdate
         /// </summary>
         public abstract void OverridableUpdate();
     }
