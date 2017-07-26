6f6885b1fd81eb936afc391da299ad6adb5d1dbc
aidandearing <aidan.dearing@live.com>
2017-07-26 07:05:58 -0400

    Prepare and Events
    Prepare is the events system, and as such I felt it fitting to make it its own 'package'.
    Event is a request, but of a specific class, with a unique logic of operation (Do must propegate the event to connected hardpoints)
    
    Up next, Memento thread safety, and refactoring locking to lock on smallest denominator instead of entire object.

---
+++ Venture/Assets/Prepare/Src/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Engine.Src;
+
+namespace Assets.Prepare.Src
+{
+    public enum Severity { Great = 0, Good = 1, Neutral = 2, Bad = 3, Terrible = 4, Catastrophic = 5 };
+
+    public abstract class Event : Request
+    {
+        public string Description { get; protected set; }
+        public Severity Severity { get; protected set; }
+    }
+}
